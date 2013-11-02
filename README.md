5268ab7ca6805f02f5795eb180a292e095d3dca1
ButuzGOL <ButuzGOL@gmail.com>
2013-11-02 12:20:19 +0200

    Init playground and prepare public

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tic tac toe
+
+It is simple multiplayer game written in JavaScript using express and socket.io
+
+## To install game:
+
+1. Download project
+2. Run `npm install`
+3. Run `bower install`
+4. Run `node app.js`
+5. Open http://localhost:9000 page in you browser
---
+++ app.js
@@ -0,0 +1,29 @@
+global.app = {
+  server: {},
+  config: require('./config'),
+  storage: require('./lib/storage')
+};
+
+var express = require('express'),
+    http = require('http'),
+    expressServer = global.app.server = express(),
+    server = http.createServer(expressServer),
+    io = (require('socket.io')).listen(server, { 'log level': 1 }),
+    listeners = require('./lib/listeners'),
+    port;
+
+port = process.env.PORT || app.config.port || 9000;
+
+server.listen(port, function() {
+  console.log('tictactoe app running on port ' + port);
+});
+
+expressServer.use('/', express.static('./public'));
+
+io.sockets.on('connection', function(socket) {
+  for (var actionName in listeners) {
+    socket.on(actionName, function(data) {
+      listeners[actionName](socket, data);
+    });
+  }
+});
