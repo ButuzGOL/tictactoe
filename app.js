718041cb1c47ac103b563c4272c01e732f4b98b4
ButuzGOL <ButuzGOL@gmail.com>
2013-11-03 12:03:52 +0200

    Jshint ready

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var express = require('express'),
     http = require('http'),
     expressServer = global.app.server = express(),
     server = http.createServer(expressServer),
-    io = (require('socket.io')).listen(server, { 'log level': 1 }),
+    io = require('socket.io').listen(server, { 'log level': 1 }),
     listeners = require('./lib/listeners'),
     port;
 
-port = process.env.PORT || app.config.port || 9000;
+port = process.env.PORT || global.app.config.port || 9000;
 
 server.listen(port, function() {
   console.log('tictactoe app running on port ' + port);
